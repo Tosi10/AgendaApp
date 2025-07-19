bfd85b92d4c0cf6585ed144c2aff3fabe8736599
Rodrigo Tosi <rodrigotosi10@gmail.com>
2025-07-19 14:04:29 -0300

    feat: NativeWind configurado e funcionando - cores personalizadas aplicadas na tela principal

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,18 +1,15 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { useGlobal } from '../../context/GlobalProvider';
 
 export default function HomeTab() {
   const { user, signOut } = useGlobal();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bem-vindo, {user?.email}!</Text>
+    <View className="flex-1 justify-center items-center bg-primary">
+      <Text className="text-secondary font-pbold text-2xl mb-5">
+        Bem-vindo, {user?.email}!
+      </Text>
       <Button title="Sair" onPress={signOut} />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
-  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
-}); 
+} 
